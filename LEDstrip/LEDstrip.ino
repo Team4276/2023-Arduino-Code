d16eff32b0a3cd08fc64652a4e320bb718754f06
Brian <btnguyen2027@gmail.com>
2023-02-16 19:29:56 -0800

    program for yellow and purple

--- LEDstrip/LEDstrip.ino
+++ LEDstrip/LEDstrip.ino
@@ -13,11 +13,21 @@ const int MODE_YELLOW_FOR_CONE = 5;
 const int MODE_UNUSED = 6;
 const int MODE_DEFAULT = 7;
 
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 7
+#define LED_COUNT 128
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   Serial.begin(9600);
   pinMode(DIO_PIN_0, INPUT_PULLUP);
   pinMode(DIO_PIN_1, INPUT_PULLUP);
   pinMode(DIO_PIN_2, INPUT_PULLUP);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to off
 }
 
 int getMode() {
@@ -52,9 +62,43 @@ void loop() {
       break;
 
     case MODE_PURPLE_FOR_CUBE:
+      // Set all LEDS to purple
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 128, 0, 128);
+      }
+      
+      strip.show(); // Display the purple color on all LEDs
+
+      delay(500); // Wait for 500 milliseconds (0.5 seconds)
+
+      // Turn all LEDs off
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 0, 0, 0);
+      }
+
+      strip.show(); // Turn off all LEDs
+
+      delay(500); // Wait for 500 milliseconds (0.5 seconds)
       break;
 
     case MODE_YELLOW_FOR_CONE:
+      // Set all LEDS to yellow
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 255, 255, 0);
+      }
+      
+      strip.show(); // Display the purple color on all LEDs
+
+      delay(500); // Wait for 500 milliseconds (0.5 seconds)
+
+      // Turn all LEDs off
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 0, 0, 0);
+      }
+
+      strip.show(); // Turn off all LEDs
+
+      delay(500); // Wait for 500 milliseconds (0.5 seconds)
       break;
 
     case MODE_UNUSED:
@@ -65,3 +109,4 @@ void loop() {
   }
 
 }
+
